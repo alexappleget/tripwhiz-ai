7dd03ccaae45c63eac8255abd4d664e2021a6f87
Alex Appleget <alexappleget2014@gmail.com>
2025-03-07 16:06:06 -0600

    Fix: created a user avatar with dropdown

--- components/UserAvatar/UserAvatar.tsx
+++ components/UserAvatar/UserAvatar.tsx
@@ -1,10 +1,40 @@
+import { PopoverTrigger } from "@radix-ui/react-popover";
+import { Popover, PopoverContent } from "../Popover/popover";
 import { Avatar, AvatarFallback, AvatarImage } from "./avatar";
+import Link from "next/link";
 
 export const UserAvatar = ({ src }: { src: string }) => {
   return (
-    <Avatar>
-      <AvatarImage src={src} alt="Your avatar photo for your profile" />
-      <AvatarFallback>CN</AvatarFallback>
-    </Avatar>
+    <Popover>
+      <PopoverTrigger asChild>
+        <Avatar className="hover:cursor-pointer">
+          <AvatarImage src={src} alt="Your avatar photo for your profile" />
+          <AvatarFallback>CN</AvatarFallback>
+        </Avatar>
+      </PopoverTrigger>
+      <PopoverContent className="w-52">
+        <div className="grid gap-4">
+          <div className="space-y-2">
+            <Link href="" className="font-medium leading-none hover:underline">
+              View Profile
+            </Link>
+          </div>
+          <div className="grid gap-2">
+            <div className="grid grid-cols-3 items-center gap-4 hover:underline">
+              <Link href="">Edit Profile</Link>
+            </div>
+            <div className="grid grid-cols-3 items-center gap-4 hover:underline">
+              <Link href="">asdf</Link>
+            </div>
+            <div className="grid grid-cols-3 items-center gap-4 hover:underline">
+              <Link href="">asdf</Link>
+            </div>
+            <div className="grid grid-cols-3 items-center gap-4 hover:underline">
+              <Link href="">Logout</Link>
+            </div>
+          </div>
+        </div>
+      </PopoverContent>
+    </Popover>
   );
 };
